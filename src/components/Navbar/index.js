d5100267ab6dcf0bcd1f4ce4236db90c2b2a8c90
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-10-30 20:38:10 +0200

    added navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import './index.css'
+import logo from '../../assets/logo.png'
 
 
 const Navbar = () => {
   return (
     <div className='navbar-container'>
-        
+        <img src={logo} className='logo'/>
+        <p>Home</p>
+        <p>Work</p>
+        <p>Contact</p>
     </div>
   )
 }
