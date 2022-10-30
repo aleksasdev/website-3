ad157760b6b94fa5d31d19fe8995f7ac813b6a8e
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-10-30 22:05:26 +0200

    refactored navbar to be inside header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from './components/Header';
-import Navbar from './components/Navbar';
 import './index.css';
 
 function App() {
   return (
     <div className="main-container">
-        <Navbar/>
         <Header/>
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './index.css'
 import noise from '../../assets/noise.png'
+import logo from '../../assets/logo.png'
 import ribbon from '../../assets/ribbon.png'
 import tablet from '../../assets/tablet.png' 
 import rod from '../../assets/rod.png' 
@@ -10,6 +11,21 @@ import titleGraphic from '../../assets/title-graphic.png'
 const Header = () => {
   return (
     <div className='header-container'>
+
+        <div className='navbar-container'>
+          <img src={logo} className='logo'/>
+          <p className='navbar-button'>Home</p>
+          <p className='navbar-button'>Work</p>
+          <p className='navbar-button'>Contact</p>
+        </div>
+
+
+        <div className='section-container'>
+          <div className='first-section'>
+            <img src={titleGraphic} className='title-graphic'/>
+          </div>
+        </div>
+
         <div className='loose-assets-container'>
             <img src={rod} className='rod' id='loose-item'/>
             <img src={ribbon} className='ribbon' id='loose-item'/>
