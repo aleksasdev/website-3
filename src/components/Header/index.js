3b13aad3e52bd655bfb17e991dc4ae4fd6d85f6b
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-10-30 22:38:04 +0200

    added first section and refactored navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,14 +15,26 @@ const Header = () => {
         <div className='navbar-container'>
           <img src={logo} className='logo'/>
           <p className='navbar-button'>Home</p>
-          <p className='navbar-button'>Work</p>
-          <p className='navbar-button'>Contact</p>
+          <p className='navbar-button'>My Projects</p>
+          <p className='navbar-button'>About Me</p>
+          <div className='navbar-contact-button'>
+            <p>My Work</p>
+          </div>
         </div>
 
 
         <div className='section-container'>
           <div className='first-section'>
             <img src={titleGraphic} className='title-graphic'/>
+            <p className='first-section-body-text'>
+              Hello, I'll keep it short and to the point, 
+              if you're in need of a website developer,
+              or someone that could create desktop software,
+              then I'm your person. Take a look at my work to get a glance 
+              at what I'm capable of and gauge whether
+              we're a good fit for each other and whether
+              I meet your competence needs.
+            </p>
           </div>
         </div>
 
